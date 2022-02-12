d0547607954d21a26ad7ddc87e759e7447a22936
nbai <nbaiinoue@gmail.com>
2022-02-12 23:18:53 +0900

    second-commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ここの文言が変更されましたよー
         </a>
       </header>
     </div>
